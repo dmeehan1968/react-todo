e9ba559fd0219bfd6c514667ff7056345cd2d274
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-17 18:37:27 +0000

    Setup basic Sketch render

--- sketch/index.sketch.js
+++ sketch/index.sketch.js
@@ -1,12 +1,36 @@
 // @flow
 import * as React from 'react'
-import { render, Artboard, Document, Page, StyleSheet } from 'react-sketchapp'
+import { render, Artboard, Document, Page, View, Text, StyleSheet } from 'react-sketchapp'
 
+const styles = StyleSheet.create({
+  document: {
+
+  },
+  page: {
+
+  },
+  artboard: {
+    height: 500,
+    width: 500,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  view: {
+
+  },
+  text: {
+
+  }
+})
 export default () => {
   render((
-    <Document>
-      <Page>
-        
+    <Document style={styles.document}>
+      <Page style={styles.page}>
+        <Artboard style={styles.artboard}>
+          <View style={styles.view}>
+            <Text style={styles.text}>Welcome to Sketch</Text>
+          </View>
+        </Artboard>
       </Page>
     </Document>
 ))
