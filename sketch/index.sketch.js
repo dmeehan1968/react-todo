e3952eb7c5fd72ea9643952d08fd77ae74b494f5
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-18 19:09:20 +0000

    Add ViewPort and setup for empty and populated lists

--- sketch/index.sketch.js
+++ sketch/index.sketch.js
@@ -2,6 +2,9 @@
 import * as React from 'react'
 import { render, Artboard, Document, Page, View, Text, StyleSheet } from 'react-sketchapp'
 
+/* eslint-disable react-native/no-color-literals */
+/* eslint-disable react-native/no-unused-styles */
+
 const styles = StyleSheet.create({
   document: {
 
@@ -10,10 +13,16 @@ const styles = StyleSheet.create({
 
   },
   artboard: {
-    height: 500,
-    width: 500,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 75,
+  },
+  viewport: {
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+  iPhoneX: {
+    backgroundColor: 'white',
+    height: 812,
+    width: 375,
   },
   view: {
 
@@ -22,14 +31,36 @@ const styles = StyleSheet.create({
 
   }
 })
+
+const ViewPort = ({ device, children }: {device: string, children?: React.Node}) => (
+  <View
+    name={'ViewPort-' + device}
+    style={styles.viewport}
+  >
+    <View
+      name="Title"
+    >
+      <Text>{device}</Text>
+    </View>
+    <View
+      name={'Content-' + device}
+      style={styles[device]}
+    >
+      {children}
+    </View>
+  </View>
+)
 export default () => {
   render((
     <Document style={styles.document}>
       <Page style={styles.page}>
-        <Artboard style={styles.artboard}>
-          <View style={styles.view}>
-            <Text style={styles.text}>Welcome to Sketch</Text>
-          </View>
+        <Artboard name="Project List" style={styles.artboard}>
+          <ViewPort device="iPhoneX">
+          </ViewPort>
+        </Artboard>
+        <Artboard name="Empty Project List" style={styles.artboard}>
+          <ViewPort device="iPhoneX">
+          </ViewPort>
         </Artboard>
       </Page>
     </Document>
