ce338beb0af79bb61c7a186525cfbc18fa4fb4c6
Dave Meehan <dave_meehan@replicated.co.uk>
2018-02-18 22:22:14 +0000

    Remove eslint disables, name component

--- src/Theme/StyledComponent.js
+++ src/Theme/StyledComponent.js
@@ -3,9 +3,7 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 
 export default (Component: React.ComponentType<any>) => (
-
-  //eslint-disable-next-line react/prefer-stateless-function, react/no-multi-comp
-  class extends React.Component<React.ElementProps<typeof Component>> {
+  class StyledComponent extends React.Component<React.ElementProps<typeof Component>> {
     static displayName = 'Styled' + (Component.displayName || Component.name || 'Component')
 
     static contextTypes = {
